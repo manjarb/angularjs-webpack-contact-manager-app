527ec89e9640807aa81f859984f09427aa4f3847
Varis Darasirikul <varis.dara@gmail.com>
2018-07-15 22:52:25 +0800

    Update app component

---
+++ src/app.module.js
@@ -0,0 +1,10 @@
+// Core Styles
+import './styles/main.scss';
+
+// Core Angular
+import angular from 'angular';
+
+const dependencies = [];
+
+angular.module('app', dependencies)
+    .component('appComponent', AppComponent)
